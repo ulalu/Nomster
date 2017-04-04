05eda5e3d44b658f9600a2a6c6639230d085a8f7
Ursula Moreno-VanderLaan <uvanderlaan@gmail.com>
2017-04-04 19:45:54 +0000

    edit and update permissions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update]
     def index
         @places = Place.paginate(page: params[:page], per_page: 10)
     end
@@ -19,6 +19,10 @@ class PlacesController < ApplicationController
     
     def edit
         @place = Place.find(params[:id])
+        
+        if @place.user !=current_user
+            return render text: 'Not Allowed', status: :forbidden
+        end
     end
     
     def update
