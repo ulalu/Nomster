081769ddc41b2fe404489c53f1be44a535d551e8
Ursula Moreno-VanderLaan <uvanderlaan@gmail.com>
2017-04-04 21:06:53 +0000

    Latitude and longitude, geocoding set up

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,9 @@
 class Place < ApplicationRecord
     belongs_to :user
-    validates :name, presence: true
-    validates :address, presence: true
-    validates :description, presence: true
+    geocoded_by :address
+    after_validation :geocode
+    
+    validates :name, length: {minimum: 3, too_short: "Your place name must include at least 3 characters" }
+    validates :address, length: {minimum: 10, too_short: "Please include the full address"}
+    validates :description, length: {in: 10...500, too_long: "500 characters is the maximum allowed", too_short: "Please include a valid description" }
 end
