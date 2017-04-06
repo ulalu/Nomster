bae4980fa351305d01c8f3f5702a58f45bcb390e
Ursula Moreno-VanderLaan <uvanderlaan@gmail.com>
2017-04-06 20:04:38 +0000

    code cleaned up, comments controller functioning, revisit validations at a later date

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,10 @@
 class CommentsController < ApplicationController
     before_action :authenticate_user!
-    
+
     def create
         @place = Place.find(params[:place_id])
         @place.comments.create(comment_params.merge(user: current_user))
-        if @comment.valid?
-            redirect_to place_path(@place)
-        else
-            render :new, status: :unprocessable_entity
-        end
+        redirect_to place_path(@place)
     end
     
     private
