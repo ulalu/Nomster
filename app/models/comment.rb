dde3ade4ad0c66fc1113d28af7b47e309dff1489
Ursula Moreno-VanderLaan <uvanderlaan@gmail.com>
2017-04-06 19:11:43 +0000

    tried to get comment submissions functioning

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :place
+    
+    validates :comment, length: {in: 5...500, too_long: "500 characters is the maximum allowed", too_short: "This field requires at least 5 characters" }
 end
