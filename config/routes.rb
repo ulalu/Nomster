5b26c6113ca15c573638cd633cbd3b012fd86946
Ursula Moreno-VanderLaan <uvanderlaan@gmail.com>
2017-04-12 02:16:33 +0000

    think i have the basics of submitting information to the photo db, and authentification and redirection. Steps I commented out: pulling the photo url onto the web page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'places#index'
   resources :places do
+    resources :photos, only: :create
     resources :comments, only: :create
   end
 end
