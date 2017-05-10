7ad1040d5da96bdb75441e4dd7643bf3fff4850e
Ursula Moreno-VanderLaan <uvanderlaan@gmail.com>
2017-05-10 19:15:13 +0000

    added sendgrid to production version

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   config.action_mailer.delivery_method = :test
+#  config.action_mailer.delivery_method = :smtp
 
 #  config.action_mailer.smtp_settings = {
 #    address: "smtp.gmail.com",
